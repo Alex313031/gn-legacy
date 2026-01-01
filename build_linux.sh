7b5a8fd5a0ed6ab6952403fe064a338683cf3e2e
Alexander Frick <Alex313031@gmail.com>
2026-01-01 00:40:49 -0600

    add linux cross compilation to win32 scripts

---
+++ build_linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Copyright (c) 2025 Alex313031.
+
+export HERE=${PWD}
+
+${HERE}/gn gen ${HERE}/out/Debug &&
+
+${HERE}/ninja -C ${HERE}/out/Debug -t clean &&
+
+${HERE}/ninja -C ${HERE}/out/Debug sfhasher -v -d all -j8 $@

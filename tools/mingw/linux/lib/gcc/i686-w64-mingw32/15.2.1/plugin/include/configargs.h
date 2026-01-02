a680f6b19aa64eec9e2a6422057851d5c4a209d1
Alexander Frick <Alex313031@gmail.com>
2026-01-02 00:21:19 -0600

    remove mingw builds in favor of unpacking .zips instead

--- tools/mingw/linux/lib/gcc/i686-w64-mingw32/15.2.1/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/home/alex/workon/mingw/build/src/gcc/configure --target=i686-w64-mingw32 --disable-shared --enable-static --disable-multilib --prefix=/home/alex/workon/mingw/build/i686 --enable-languages=c,c++ --disable-nls --enable-threads=posix --disable-sjlj-exceptions --with-dwarf2";
-static const char thread_model[] = "posix";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "cpu", "generic" }, { "arch_32", "pentiumpro" }, { "arch_64", "x86-64" } };

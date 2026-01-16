83cfd3cd571de60e5fc958712806ca337cac5519
Alexander Frick <Alex313031@gmail.com>
2026-01-16 15:59:51 -0600

    update cryocalc to 0.0.4 beta

--- build_linux.sh
+++ build_linux.sh
@@ -8,4 +8,10 @@ ${HERE}/gn gen ${HERE}/out/Debug &&
 
 ${HERE}/ninja -C ${HERE}/out/Debug -t clean &&
 
-${HERE}/ninja -C ${HERE}/out/Debug all -v -d stats -j8 $@
+${HERE}/ninja -C ${HERE}/out/Debug all -v -d stats -j8
+
+${HERE}/gn gen ${HERE}/out/Release &&
+
+${HERE}/ninja -C ${HERE}/out/Release -t clean &&
+
+${HERE}/ninja -C ${HERE}/out/Release all -v -d stats -j8

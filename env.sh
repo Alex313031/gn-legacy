36eedce3a187f3826846f753725ea124b35bccc3
Alexander Frick <Alex313031@gmail.com>
2026-02-13 01:50:39 -0600

    update cryocalc submodule with osinfo 1.5.2, remove stale env.sh file

--- env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Copyright (c) 2025 Alex313031.
-
-export HERE=${PWD} &&
-export PATH=${HERE}/tools/llvm-mingw-20251202-msvcrt-ubuntu-22.04-x86_64/bin/:$PATH &&
-export PATH=${HERE}/:$PATH
